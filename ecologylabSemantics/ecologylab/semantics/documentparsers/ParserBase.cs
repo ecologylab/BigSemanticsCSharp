55799f7408d54596b25975e05f5228b9c75a482e
quyin <quyin@ecologylab.net>
2011-10-24 21:40:30 +0000

    copy missing DLLs (icudt42.dll) to target projects.

---
+++ ecologylabSemantics/ecologylab/semantics/documentparsers/ParserBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Simpl.Fundamental.Net;
+
+namespace ecologylab.semantics.documentparsers
+{
+    abstract class ParserBase : DocumentParser
+    {
+
+        private ParsedUri _puri;
+
+        public ParserBase(ParsedUri puri)
+        {
+            _puri = puri;
+        }
+
+        public override void Parse()
+        {
+            // connect, handle redirect, select meta-metadata
+
+            throw new NotImplementedException();
+        }
+
+        public override void PostParse()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
