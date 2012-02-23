2b490de9c6b16b2995f1236c161285ddbf867268
damaraju <damaraju@ecologylab.net>
2012-02-23 17:59:57 +0000

    Copies exist in new top level directories. Deleting from here.

--- ecologylabInteractive/ecologylab/interactive/Utils/TouchDelegates.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Windows.Input;
-
-namespace ecologylabInteractiveSemantics.ecologylab.interactive.Utils
-{
-    public delegate void TouchDelegate(object sender, TouchEventArgs args);
-}
