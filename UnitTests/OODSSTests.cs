4ae6db7ebd6ee184f846ba8515ddfbcf9a215b9e
dincadragosh <dincadragosh@ecologylab.net>
2012-07-18 18:15:14 +0000

    Added in OODSSTests a test method used only to see the comunication with the server - TestMetadataServicesComunication().

--- UnitTests/OODSSTests.cs
+++ UnitTests/OODSSTests.cs
@@ -6,12 +6,42 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ecologylab.semantics.generated.library;
 using ecologylab.semantics.metadata.builtins;
 using ecologylab.semantics.services;
+using System.Net;
+using System.Net.Sockets;
 
 namespace UnitTests
 {
     [TestClass]
     public class OODSSTests
     {
+        [TestMethod]
+        public void TestMetadataServicesComunication()
+        {
+            string Host = "127.0.0.1";
+            int Port = 2107;
+            Socket _clientSocket;
+
+            byte[] _response = new byte[2048];
+
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Host);//"achilles.cse.tamu.edu");
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, Port);
+            _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _clientSocket.Connect(Host, Port);
+
+            Console.WriteLine("Client socket with host ({0}) connected? {1}.", Host, _clientSocket.Connected);
+
+            _clientSocket.Send(Encoding.ASCII.GetBytes("content-length:26\r\nuid:1\r\n\r\n<init_connection_request/>"));
+
+            //_clientSocket.Receive(_response);
+            //Console.WriteLine(_response.ToString());
+
+            _clientSocket.Send(Encoding.ASCII.GetBytes("content-length:70\r\nuid:1\r\n\r\n<metadata_request url=\"http://www.amazon.com/gp/product/B0050SYS5A/\"/>"));
+
+            //_clientSocket.Receive(_response);
+            //Console.WriteLine(_response.ToString());
+        }
+
         [TestMethod]
         public async void TestMetadataServicesClient()
         {
